d4c168b8d00b6a912004a208e5dbe53d9f35b218
jhounsell <151621173+jhounsell@users.noreply.github.com>
2024-03-07 15:02:17 +0000

    Where are the exploiters now question build

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,24 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+//know-where-router
+
+router.all('/know-where-router', function (req, res, next) {
+    if ((req.session.data['know-where'] == 'yes')) {
+      return res.redirect('/UK-question');
+      next();
+    } else {
+      return (res.redirect('/Q1-end-page'))
+    }});
+
+//UK-question-router
+
+router.all('/UK-question-router', function (req, res, next) {
+    if ((req.session.data['uk-question'] === 'no')) {
+      return res.redirect('/outside-UK');
+      next();
+    } else {
+      return (res.redirect('/inside-UK'))
+    }});
